5c24960126550fc565e495652676b769b529bbf7
ishankfl <ishankafle5@gmail.com>
2026-06-13 20:23:20 +0545

    email-setting-added

--- EcommerceApp.Domain/Configurations/EmailSettings.cs
+++ EcommerceApp.Domain/Configurations/EmailSettings.cs
@@ -2,7 +2,6 @@
 
 public class EmailSettings
 {
-    // SMTP Settings
     public string SmtpServer { get; set; } = string.Empty;
     public int SmtpPort { get; set; } = 587;
     public string SenderEmail { get; set; } = string.Empty;
@@ -11,18 +10,4 @@ public class EmailSettings
     public string Password { get; set; } = string.Empty;
     public bool EnableSsl { get; set; } = true;
     public int Timeout { get; set; } = 10000;
-
-    // Feature Toggles
-    public bool EnableEmailVerification { get; set; } = true;
-    public bool EnableLoginNotification { get; set; } = true;
-    public bool EnableWelcomeEmail { get; set; } = true;
-
-    // Token Expiry (in hours)
-    public int EmailVerificationTokenExpiryHours { get; set; } = 24;
-    public int PasswordResetTokenExpiryHours { get; set; } = 1;
-    public int PhoneVerificationTokenExpiryMinutes { get; set; } = 10;
-
-    // Security Settings
-    public int MaxLoginAttempts { get; set; } = 5;
-    public int LockoutDurationMinutes { get; set; } = 15;
 }
